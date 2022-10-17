f14031c2869f91a624d0f1a7110fd1c166ca9911
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-17 09:32:47 -0300

    Informaçoes na página "visualizar" e css

--- src/pages/AddEdit.jsx
+++ src/pages/AddEdit.jsx
@@ -1,141 +1,114 @@
-import React, {useState, useEffect} from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import fireDb from '../firebase';
-import './AddEdit.css'
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import fireDb from "../firebase";
+import "./AddEdit.css";
 
 const initialState = {
   title: "",
   description: "",
-  fullDescription: ""
-}
+  fullDescription: "",
+};
 
 const AddEdit = () => {
-
-  const [state, setState] = useState(initialState)
-  const [data, setData] = useState({})
-  const {title, description, fullDescription} = state
-  const navigate = useNavigate()
-  const {id} = useParams()
+  const [state, setState] = useState(initialState);
+  const [data, setData] = useState({});
+  const { title, description, fullDescription } = state;
+  const navigate = useNavigate();
+  const { id } = useParams();
 
   useEffect(() => {
     fireDb.child("tasks").on("value", (snapshop) => {
-      if (snapshop.val() !== null) 
-      {
-        setData({...snapshop.val()})
+      if (snapshop.val() !== null) {
+        setData({ ...snapshop.val() });
+      } else {
+        setData({});
       }
-
-      else
-      {
-        setData({})
-      }
-    })
+    });
 
     return () => {
-      setData({})
-    }
-  }, [id])
-  
+      setData({});
+    };
+  }, [id]);
+
   useEffect(() => {
-    if(id) 
-    {
-      setState({...data[id]})
-    }
-
-    else
-    {
-      setState({...initialState})
+    if (id) {
+      setState({ ...data[id] });
+    } else {
+      setState({ ...initialState });
     }
 
     return () => {
-      setState({...initialState})
-    }
-
-  }, [id, data])
-
+      setState({ ...initialState });
+    };
+  }, [id, data]);
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target
-    setState({...state, [name]: value})
-  }
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(!title || !description || !fullDescription)
-    {
-      toast.error("Por favor preencher todas as informações.")
-    }
-
-    else
-    {
-      if (!id) 
-      {
+    e.preventDefault();
+    if (!title || !description || !fullDescription) {
+      toast.error("Por favor preencher todas as informações.");
+    } else {
+      if (!id) {
         fireDb.child("tasks").push(state, (error) => {
-          if(error)
-          {
-            toast.error(error)
-          }
-  
-          else
-          {
+          if (error) {
+            toast.error(error);
+          } else {
             toast.success("Tarefa adicionada com sucesso.");
           }
-        })
-      }
-
-      else
-      {
+        });
+      } else {
         fireDb.child(`tasks/${id}`).set(state, (error) => {
-          if(error)
-          {
-            toast.error(error)
-          }
-  
-          else
-          {
+          if (error) {
+            toast.error(error);
+          } else {
             toast.success("Tarefa Atualizada com sucesso.");
           }
-        })
+        });
       }
-    
+
       setTimeout(() => navigate("/"), 500);
     }
-  }
+  };
 
   return (
     <div className="content">
-        <form className='form' onSubmit={handleSubmit}>
-          <label htmlFor="title">Título</label>
-          <input 
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Título tarefa"
-            value={title || ""}
-            onChange={handleInputChange}
-             />
-          <label htmlFor="description">Descrição</label>
-          <input 
-            type="text"
-            id="description"
-            name="description"
-            placeholder="Breve descrição tarefa"
-            value={description || ""}
-            onChange={handleInputChange}
-             />
-          <label htmlFor="fullDescription">Descrição detalhada</label>
-          <input 
-            type="text"
-            id="fullDescription"
-            name="fullDescription"
-            placeholder="Descrição completa tarefa"
-            value={fullDescription || ""}
-            onChange={handleInputChange}
-             />
-          <input type="submit" value={id ? "Atualizar" : "Salvar"} />
-        </form>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="title">Título</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Título tarefa"
+          value={title || ""}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="description">Descrição</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Breve descrição tarefa"
+          value={description || ""}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="fullDescription">Descrição detalhada</label>
+        <input
+          type="text"
+          id="fullDescription"
+          name="fullDescription"
+          placeholder="Descrição completa tarefa"
+          value={fullDescription || ""}
+          onChange={handleInputChange}
+        />
+        <input type="submit" value={id ? "Atualizar" : "Salvar"} />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddEdit
+export default AddEdit;
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import fireDb from '../firebase';
+import { useParams, Link } from 'react-router-dom';
+import './View.css'
 
 const View = () => {
+
+  const [task, setTask] = useState({})
+  const {id} = useParams()
+
+  useEffect(() => {
+    fireDb
+    .child(`tasks/${id}`)
+    .get()
+    .then((snapshot) => {
+      if(snapshot.exists())
+      {
+        setTask({...snapshot.val()})
+      }
+
+      else
+      {
+        setTask({})
+      }
+    })
+  }, [id])
+  
+  console.log("task", task)
+
   return (
-    <div>
-        <h2>View</h2>
+    <div className="content">
+        <div className="card">
+          <div className="cardHeader">
+            <p>Detalhes da tarefa</p>
+          </div>
+          <div className="container">
+            <strong>ID: </strong>
+            <span>{id}</span>
+            <br />
+            <br />
+            <strong>Título: </strong>
+            <span>{task.title}</span>
+            <br />
+            <br />
+            <strong>Descrição: </strong>
+            <span>{task.description}</span>
+            <br />
+            <br />
+            <strong>Descrição completa: </strong>
+            <span>{task.fullDescription}</span>
+            <br />
+            <br />
+            <Link to="/">
+              <button className="btn btnEdit">Voltar</button>
+            </Link>
+          </div>
+        </div>
     </div>
   )
 }
