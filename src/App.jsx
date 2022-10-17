b139c6d4432c69132ee0b678503da760b9fc77da
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-16 23:46:20 -0300

    Navbar inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import About from './pages/About';
-import AddEdit from './pages/AddEdit';
-import Home from './pages/Home';
-import View from './pages/View';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import About from "./pages/About";
+import AddEdit from "./pages/AddEdit";
+import Home from "./pages/Home";
+import View from "./pages/View";
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ToastContainer position="top-center"/>
-      <Routes>
+        <Header/>
+        <ToastContainer position="top-center" />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<AddEdit />} />
           <Route path="/update/:id" element={<AddEdit />} />
           <Route path="/view/:id" element={<View />} />
-          <Route path="/about" element={<About />}/>
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+
+  return (
+    <div className="header">
+      <p className="logo">Tasks</p>
+      <div className="headerRight">
+        <Link to="/">
+          <p
+            className={`${activeTab === "Home" ? "active" : ""}`}
+            onClick={() => setActiveTab("Home")}
+          >
+            Home
+          </p>
+        </Link>
+        <Link to="/add">
+          <p
+            className={`${activeTab === "AddTask" ? "active" : ""}`}
+            onClick={() => setActiveTab("AddTask")}
+          >
+            Adicionar Tarefa
+          </p>
+        </Link>
+        <Link to="/about">
+          <p
+            className={`${activeTab === "About" ? "active" : ""}`}
+            onClick={() => setActiveTab("About")}
+          >
+            Sobre
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
