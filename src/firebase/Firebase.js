dbf0fab7e0db3083e0ae94e71d8b4290ba35672d
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-16 22:43:42 -0300

    Configuração inicial Firebase

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBY4ugi1P-6raVIXBkSOMNZgR9uXc-wEIs",
+  
+    authDomain: "desafio-diel-tasks.firebaseapp.com",
+  
+    databaseURL: "https://desafio-diel-tasks-default-rtdb.firebaseio.com",
+  
+    projectId: "desafio-diel-tasks",
+  
+    storageBucket: "desafio-diel-tasks.appspot.com",
+  
+    messagingSenderId: "640403504568",
+  
+    appId: "1:640403504568:web:5d88d56f08a7e057e8234d"
+  
+  };
+   
+
+  const fireDb = firebase.initializeApp(firebaseConfig);
+
+  export default fireDb.database().ref();
+  
