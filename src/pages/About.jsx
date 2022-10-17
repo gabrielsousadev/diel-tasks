d9fcb255cd7987a7846a33cc66f74abda0ce3ede
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-17 09:39:21 -0300

    Simples tela sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-        <h2>About</h2>
+    <div className="content">
+      <h2>
+        {""}
+        Este é um projeto de cadastro de tarefas, utilizando React e Firebase como banco de dados.
+      </h2>
     </div>
   )
 }
